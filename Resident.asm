b690a965a3995160c2c514b005100b7a9d07dc7d
Artiom_Windows <Artiom.shaff07@gmail.com>
2025-03-12 23:03:08 +0300

    Regs out addr via stack, more regs for out

--- Resident.asm
+++ Resident.asm
@@ -15,6 +15,8 @@ Y	equ 1
 ON_KEY	equ	57h ; f11
 OFF_KEY	equ	58h	; F12
 
+N_PUSHED_REGS equ 9d
+
 Main:
 	mov ax, offset Resident_int_9
 	mov bx, 09h	 ; Interrupt num
@@ -40,11 +42,11 @@ Hook_interrupt	proc
 	xor cx, cx
 	mov ds, cx
 	shl bx, 2
-	mov si, bx	; ds:[si] = 0000h:[n_interrupt * 4]
+	mov si, bx			; ds:[si] = 0000h:[n_interrupt * 4]
 
 	cli
-	movsw 		; copy offset (es:[di+=2] = ds:[si+=2])
-	movsw		; copy segment
+	movsw 				; copy offset (es:[di+=2] = ds:[si+=2])
+	movsw				; copy segment
 
 	mov ds:[bx], ax		; replace sys_int to new_int
 	mov ds:[bx + 2], cs
@@ -65,10 +67,10 @@ Resident_int_9	proc
 		je	Turn_off
 
 		pop	es ax
-		db 0eah		; far jump
+		db 0eah			; far jump
 Original_int_9:
-		dw 0		; offset
-		dw 0		; segment
+		dw 0			; offset
+		dw 0			; segment
 
 Turn_on:
 		mov	al, 1
@@ -96,89 +98,137 @@ Resident_int_9 endp
 ;---------------------------------
 
 Resident_int_8 proc
-	push dx cx bx ax di si bp sp ds ss es
-
-	mov byte ptr cs:[AX_REG + 4], ah 
-	mov byte ptr cs:[AX_REG + 6], al 
-	mov byte ptr cs:[BX_REG + 4], bh
-	mov byte ptr cs:[BX_REG + 6], bl
-	mov byte ptr cs:[CX_REG + 4], ch
-	mov byte ptr cs:[CX_REG + 6], cl
-	mov byte ptr cs:[DX_REG + 4], dh
-	mov byte ptr cs:[DX_REG + 6], dl 
+	push ax bx cx dx di si ds ss es
 
 	xor al, al
 	mov	al, byte ptr cs:[Active]
-	cmp al, 0
-	je	Resident_int_8_end		; if (Active == 0) return
-	
-	mov cx, 4h
-	mov si, offset AX_REG
-ChangeToAscii:
-	add si, 4
-	mov byte ptr bl, cs:[si]
-	call GetAscii
-	mov word ptr cs:[si], ax
-	add si, 2
-	mov byte ptr bl, cs:[si]
-	call GetAscii
-	mov word ptr cs:[si], ax
-	add si, 2
-	loop ChangeToAscii
+	cmp al, 0						; if (Active == 0)
+	je	Resident_int_8_end			;  return;
+
+	mov si, sp
+	add si, N_PUSHED_REGS * 2d - 1  ; 9 regs * 2 bytes - 1
+	call CopyRegs
 
 	push cs
-	pop	ds						; ds = cs
+	pop	ds							; ds = code segment
+	mov bx, VIDEOSEG
+	mov es, bx						; es = VIDEOSEG
 
-	call	DrawFrame
-	call 	DrawRegs
+	call DrawFrame
+	call DrawRegs
 
 Resident_int_8_end:	
-	pop es ss ds sp bp si di ax bx cx dx
+	pop es ss ds si di dx cx bx ax
 	db 0eah ; far jump
 	Original_int_8 DD 0	; label (offset + segment)
 
 Resident_int_8 endp
 
 ;---------------------------------
-;Entry: AH - color,
-;	DI - coords,
-;	ES - videoseg (0b800h),
-;	SI - framestyle array ptr,
-; 
-;Destr: AL - symbol, 
-;	CX - counter & line lenght
+; Entry: si - stack pointer to first pushed reg
+; Destr: di, ds, es, cx, si
 ;---------------------------------
-DrawLine 	proc
+CopyRegs proc
+	mov di, offset REG_ARR
 
-		lodsb			; al = ds:[si++]
-		stosw			; es:[di] = ax, di+=2
+	push cs ss			; ds = stack segment
+	pop ds es			; es = code segment
 
-		mov CX, FRAMELEN - 2
-		lodsb
-		rep stosw
-	
-		lodsb
-		stosw
+	mov cx, N_PUSHED_REGS
+CopyRegsLoop:			;<-------
+	add di, 4d			;		|
+	std					;		|
+	lodsb				;		|
+	cld					;		|
+	call GetAscii		;		|
+	stosw				;		|
+;						;		|
+	std					;		|
+	lodsb				;		|
+	cld					;		|
+	call GetAscii		;		|
+	stosw				;		|
+;						;		|
+	loop CopyRegsLoop	;--------
+	ret
+CopyRegs endp
+
+;---------------------------------
+; Entry: es - VIDEOSEG,
+;		 ds - code segment
+; Destr: ax, cx, si, di
+;---------------------------------
+DrawRegs proc
+
+	mov si, offset REG_ARR
+	mov ah, COLOR
+
+	mov cx, N_PUSHED_REGS
+	mov di, SCREENLEN*(Y+1)+(X+1)*2
+DrawRegsLoop:						;<-----
+	push cx							;	  |
+;									;	  |
+	mov cx, 8h						;	  |
+DrawRegLoop:						;<--  |
+	lodsb							;  |  |
+	stosw							;  |  |
+	loop DrawRegLoop				;---  |
+;									;	  |
+	add di, SCREENLEN - 8h * 2h		;     |
+	pop cx							;	  |
+	loop DrawRegsLoop				;------
+
+	ret
+DrawRegs endp
+;---------------------------------
+; Input:  al - **1 byte** of register
+; Output: ax - ascii symb of register byte
+; Destr:  bl - temp buffer
+;---------------------------------
+GetAscii	proc
+		
+		mov bl, al 		; save byte
+		and al, 0Fh		; change to ascii first hex digit (4 bits)
+		call HexToAscii	
+		mov ah, al		
+
+		mov al, bl		; change to ascii second hex digit
+		shr al, 4
+		call HexToAscii
 
 		ret
-		endp
-
+		endp	
 ;---------------------------------
-;Entry:	None
+; Input:  al - hex digit (4 bits)
+; Output: al - ascii digit byte (8 bits)
+;---------------------------------
+HexToAscii	proc
+		
+		cmp al, 9
+
+		jle IfDigitBody
+		jg  IfLetterBody
+
+IfDigitBody: 	add al, '0'
+		ret
+
+IfLetterBody:   add al, 'a' - 10
+		ret
+		
+		endp
+;---------------------------------
+;Entry:	ES - videoseg (0b800h)
 ;
 ;Destr: CX - counter,
 ;	AL - symbol,
 ;	AH - color,
 ;	DI - coords (screenlen * y + x * 2),
-;	ES - videoseg (0b800h),
 ;	SI - framestyle array ptr
 ;---------------------------------
 DrawFrame	proc
 
 		mov ah, COLOR
 		mov DI, SCREENLEN*Y+X*2
-		mov bx, VIDEOSEG
-		mov es, bx
 		mov si, offset STYLE
 		push cs
 		pop ds
@@ -211,129 +261,67 @@ EndFrameBody:
 
 		ret
 		endp
-;---------------------------------
-
-CopyRegs proc
-
-	mov	si, sp
-	add	si, 8d * 2d	+ 1
-
-	mov di, offset AX_REG
-
-	push cs cs
-	pop ds es
-
-	mov cx, 4d
-CopyRegsLoop:
-	add di, 4d
-	mov bl, ds:[si]
-	call GetAscii
-	stosw
-	add si, 1d
-	mov bl, ds:[si]
-	call GetAscii
-	stosw
-	add si, 1d
-
-	loop CopyRegsLoop
-
-	ret
-CopyRegs endp
 
 ;---------------------------------
-
-DrawRegs proc
-
-	push cs
-	pop ds
-	mov si, offset AX_REG
-
-	mov ah, COLOR
-
-	mov bx, VIDEOSEG
-	mov es, bx
-
-	mov cx, 8h
-	mov di, SCREENLEN*(Y+1)+(X+1)*2
-DRAW_AX:
-	lodsb
-	stosw
-	loop DRAW_AX
-
-	mov cx, 8h
-	mov di, SCREENLEN*(Y+2)+(X+1)*2
-DRAW_BX:
-	lodsb
-	stosw
-	loop DRAW_BX
-
-	mov cx, 8h
-	mov di, SCREENLEN*(Y+3)+(X+1)*2
-DRAW_CX:
-	lodsb
-	stosw
-	loop DRAW_CX
-
-	mov cx, 8h
-	mov di, SCREENLEN*(Y+4)+(X+1)*2
-DRAW_DX:
-	lodsb
-	stosw
-	loop DRAW_DX
-
-	ret
-DrawRegs endp
+;Entry: AH - color,
+;	DI - coords,
+;	ES - videoseg (0b800h),
+;	SI - framestyle array ptr,
+; 
+;Destr: AL - symbol, 
+;	CX - counter & line lenght
 ;---------------------------------
-;Input: bl - 1 byte of register
-GetAscii	proc
-		
-		mov al, bl
-		and al, 0Fh
-		call HexToAscii
-		mov ah, al
+DrawLine 	proc
 
-		mov al, bl
-		shr al, 4
-		call HexToAscii
+		lodsb			; al = ds:[si++]
+		stosw			; es:[di] = ax; di+=2
+
+		mov CX, FRAMELEN - 2
+		lodsb
+		rep stosw
+	
+		lodsb
+		stosw
 
 		ret
-		endp	
-;---------------------------------
-
-HexToAscii	proc
-		
-		cmp al, 9
-
-		jle IfDigitBody
-		jg  IfLetterBody
-
-IfDigitBody: 	add al, '0'
-		ret
-
-IfLetterBody:   add al, 'a' - 10
-		ret
-		
 		endp
+
 ;---------------------------------
 
 Exit: mov ax, 3100h
 	mov dx, offset EOP
-	shr dx, 4 		; dx /= 16
+	shr dx, 4 			; dx /= 16
 	inc dx
 	int 21h
+;---------------------------------
 
 Active db 0
-STYLE	db 218, 196, 191, 179, , 179, 192, 196, 217 ; (Ú, Ä, ¿, ³, , ³, À, Ä, Ù)
-AX_REG	db "AX: " 
-dd 0
-BX_REG	db "BX: "
-dd 0
-CX_REG	db "CX: " 
-dd 0 
-DX_REG	db "DX: " 
-dd 0
+STYLE	db 218, 196, 191, 179, , 179, 192, 196, 217 ; (â”Œ, â”€, â”, â”‚, , â”‚, â””, â”€, â”˜)
 
 HexTable: db "0123456789abcdef"
+
+REG_ARR	db "AX: "
+dd 0
+db "BX: "
+dd 0
+db "CX: " 
+dd 0 
+db "DX: " 
+dd 0
+db "DI: "
+dd 0
+db "SI: " 
+dd 0
+db "DS: " 
+dd 0
+db "SS: " 
+dd 0
+db "ES: " 
+dd 0
+db "CS: " 
+dd 0
+db "IP: " 
+dd 0
 
 EOP:
 end 	Main
--- Test.asm
+++ Test.asm
@@ -3,20 +3,29 @@
 org 100h
 
 Start:
-    mov bx, 1111h
-    mov cx, 2022h
-    mov dx, 3333h
+    	mov bx, 1111h
+    	mov cx, 2022h
+    	mov dx, 3333h
+    	mov di, 4444h
+    	mov si, 5555h
+    	push 6666h
+	pop ds
+	push 7777h
+	pop ss
+	push 8888h
+	pop es
+		
 
-    mov ah, 1h
-    int 16h
-    jnz Exit
+    	mov ah, 1h
+    	int 16h
+    	jnz Exit
 
-    jmp Start
+    	jmp Start
 
 Exit:
-    mov ah, 4ch
-    xor al, al
-    int 21h
+    	mov ah, 4ch
+    	xor al, al
+    	int 21h
 
 
 END Start
